470bb625d83b79a992afdff34cdac6d4e174a419
Daniel Thomas <drt24@cam.ac.uk>
2014-08-08 20:14:55 +0100

    Count linux versions

--- countversions.sh
+++ countversions.sh
@@ -4,18 +4,25 @@ set -e
 
 start_date='2011-07-01'
 end_date='2014-07-31'
-openssl_count=0
-output=`wget --quiet -O - https://www.openssl.org/source/ | grep '.tar.gz' | cut -d'<' -f 1 |  sed 's/^ [0-9]\+ //' | tr ' ' '_'`
-for i in $output
-do
-    i=`echo $i | tr '_' ' '`
-    d=`date --date="$i" --rfc-3339='date'`
-    if [[ $d > $start_date ]]
-    then
-        if [[ $d < $end_date  ]]
+function do_count {
+    name=$1
+    output=$2
+    count=0
+    for i in $output
+    do
+        i=`echo $i | tr '_' ' '`
+        d=`date --date="$i" --rfc-3339='date'`
+        if [[ $d > $start_date ]]
         then
-            openssl_count=$((openssl_count+1))
+            if [[ $d < $end_date  ]]
+            then
+                count=$((count+1))
+            fi
         fi
-    fi
-done
-echo "\\newcommand{\\opensslNumVersions}{$openssl_count}"
+    done
+    echo "\\newcommand{\\${name}NumVersions}{$count}"
+}
+do_count 'openssl' "`wget --quiet -O - https://www.openssl.org/source/ | grep '.tar.gz' | cut -d'<' -f 1 |  sed 's/^ [0-9]\+ //' | tr ' ' '_'`"
+do_count 'linux' "`wget --quiet -O - https://www.kernel.org/pub/linux/kernel/v2.6/ | grep 'linux-' | grep '.tar.gz' | tr -s ' ' | cut -d' ' -f 3`
+`wget --quiet -O - https://www.kernel.org/pub/linux/kernel/v3.0/ | grep 'linux-' | grep '.tar.gz' | tr -s ' ' | cut -d' ' -f 3`"
+
