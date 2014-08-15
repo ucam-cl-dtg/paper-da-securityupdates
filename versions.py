27eca16a4ddf2321d09f0f8343723566c8082869
Daniel Thomas <drt24@cam.ac.uk>
2014-08-15 12:33:38 +0100

    Fetch bouncycastle version date information

--- versions.py
+++ versions.py
@@ -15,6 +15,7 @@ latex_value.latex_value_filename('countversions.tex')
 from dahelpers import da_start_date, da_end_date, warning, save_csv_list_file, load_csv_list_file
 from collections import OrderedDict
 from uncertainties import ufloat
+from lxml import html
 
 def get_lines(address, matcher):
     response = urllib.request.urlopen(address)
@@ -37,12 +38,42 @@ def get_version_dates(lines, version_matcher, date_matcher):
             warning('Unmatched line: ', line, m_version, m_date)
     return version_dates
 
+
+def fetch_bouncy_castle():
+    # Older data is available from http://freecode.com/projects/bouncycastlecryptoapi/releases should we need it, but it lacks newer data
+    urls = ['http://bouncy-castle.1462172.n4.nabble.com/Bouncy-Castle-Announce-f1468408.html', 'http://bouncy-castle.1462172.n4.nabble.com/Bouncy-Castle-Announce-f1468408i35.html']
+    bouncy_version_matcher = re.compile('.*((Bouncy Castle Crypto Provider Package version)|(BC Release)|(Java Release)) ([0-9.]+) (is )?now available.*')
+    bouncy_date_matcher = re.compile(r'.*new Date\((\d+)\).*', flags=re.DOTALL)
+    version_dates = []
+    for url in urls:
+        response = urllib.request.urlopen(url)
+        tree = html.fromstring(response.read())
+        table = tree.xpath('/html/body/div/table[1]')[0]
+        rows = table.getchildren()
+        for row in rows:
+            if not 'main' in row.get('class'):
+                continue
+            cells = row.getchildren()
+            subject = cells[1].xpath('table/tr/td/a/text()')[0]
+            match = bouncy_version_matcher.match(subject)
+            if not match:
+                continue
+            version = match.group(5)
+            date_script = cells[3].xpath('table/tr/td/a/script/text()')[0]
+            match = bouncy_date_matcher.match(date_script)
+            if not match:
+                continue
+            date = datetime.datetime.fromtimestamp(int(match.group(1)[:-3])).date()
+            version_dates.append((version, date))
+    return version_dates
+
 def num_in_range(version_dates):
     num = 0
     for _, date in version_dates:
         if date > da_start_date and date < da_end_date:
             num +=1
     return num
+
 
 def save_to_csv(name, version_dates):
     save_csv_list_file(map(lambda x : [x[0], x[1].isoformat()], version_dates),'version_dates_' + name + '.csv')
@@ -63,7 +94,12 @@ def update_versions():
     linux_version_dates = get_version_dates(linux_lines, re.compile(r'.*"linux-([0-9.]+)\.tar\.gz".*'), re.compile('.*?\s+\s([0-9\-A-z: ]+)\s+\d+M.*'))
     set_latex_value('linuxNumVersions', num_in_range(linux_version_dates))
     save_to_csv('linux', linux_version_dates)
-    return {'openssl': openssl_version_dates, 'linux' : linux_version_dates}
+
+    bouncycastle_version_dates = fetch_bouncy_castle()
+    set_latex_value('bouncycastleNumVersions', num_in_range(bouncycastle_version_dates))
+    save_to_csv('bouncycastle', bouncycastle_version_dates)
+
+    return {'openssl': OrderedDict(openssl_version_dates), 'linux' : OrderedDict(linux_version_dates), 'bouncycastle': OrderedDict(bouncycastle_version_dates)}
 
 
 def load_versions():
