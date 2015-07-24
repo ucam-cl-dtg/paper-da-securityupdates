6cc048aba05e1416c151bf9616bf9c97851c1926
Daniel Thomas <drt24@cam.ac.uk>
2015-07-24 11:45:53 +0100

    Add script to generate the spsm source submission

---
+++ spsm-submission.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp spsm04-thomas.{pdf,tex} spsm-submission/
+cp security_scoring.tex spsm-submission/
+cp countversions.tex spsm-submission/
+cp avostats.tex spsm-submission/
+cp dastats.tex spsm-submission/
+cp -R figures spsm-submission/
+~/git/papers/linuxconfig/bin/bibtex-filter.py --input-bibtex securityupdates.bib --input-aux spsm04-thomas.aux --output-bibtex spsm-submission/securityupdates.bib
